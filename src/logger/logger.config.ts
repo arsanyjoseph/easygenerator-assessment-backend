9e5cdbb0b0fde2df4b8a716a37984674fabbb859
arsanyBenyamine <arsany.benyamine@yomicepa.com>
2025-03-03 23:04:34 +0200

    add logger config

---
+++ src/logger/logger.config.ts
@@ -0,0 +1,42 @@
+import { WinstonModuleOptions } from 'nest-winston';
+import * as winston from 'winston';
+import 'winston-daily-rotate-file';
+
+export const winstonConfig: WinstonModuleOptions = {
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.colorize(),
+        winston.format.printf(({ timestamp, level, message, context }) => {
+          return `${timestamp} [${context}] ${level}: ${message}`;
+        }),
+      ),
+    }),
+    
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/error-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      level: 'error',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+    
+    new winston.transports.DailyRotateFile({
+      filename: 'logs/application-%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+      format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+      ),
+    }),
+  ],
+};
